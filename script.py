be1364938abd63c0698a8ef557fbfc01f3d58c8b
Mohamed <adan.Mohamed.w@gmail.com>
2025-05-26 20:53:46 -0500

    credit report dashboard

---
+++ script.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+data = pd.read_csv('credit.csv')
+
+# Display the first few rows of the data
+print(data.head())
